27e0eb4391937d286a8af6add93318f19e6f6b07
SagaOfAGuy <68156940+SagaOfAGuy@users.noreply.github.com>
2021-01-16 02:24:22 -0500

    Create ChangePassword.bat

---
+++ ChangePassword.bat
@@ -0,0 +1,18 @@
+:: After booting into the main partition, click on the "Ease of Access" button on the lower right-hand size of the Windows login screen
+
+:: Doing this should bring up a CMD window
+
+:: Make sure current directory is C:\Windows\system32
+cd C:\Windows\system32
+
+:: This will bring up password reset screen
+control userpasword2
+
+:: Reset the password via the password reset screen
+
+:: exit CMD prompt
+exit 
+
+:: Enter the password, and login
+
+
